ade92f6a87fe4d59f61947a01d266d4f151be255
wjh <wjh061016@gmail.com>
2025-06-14 11:59:34 +0900

    250614

--- 1018.cpp
+++ 1018.cpp
@@ -2,6 +2,19 @@
 #include <vector>
 using namespace std;
 
+int check(const vector<string> s, int x, int y){
+	int a=0,b=0;
+	for(int i=x;i<x+8;i++){
+		for(int j=y;j<y+8;j++){
+			if(s[i][j]=='W'&&(i+j)%2==0) a++;
+			else if(s[i][j]=='B'&&(i+j)%2==1) a++;
+			if(s[i][j]=='B'&&(i+j)%2==0) b++;
+			else if(s[i][j]=='W'&&(i+j)%2==1) b++;
+		}
+	}
+	return max(a,b);
+}
+
 int main() {
 	ios::sync_with_stdio(false);
 	cin.tie(NULL);
@@ -12,6 +25,12 @@ int main() {
 	for(int i=0;i<N;i++){
 		cin >> A[i];
 	}
-	 
+	int sq=0;
+	for(int i=0;i<N-7;i++){
+		for(int j=0;j<M-7;j++){
+			sq = max(sq,check(A,i,j));
+		}
+	}
+	cout << 64-sq;
 	return 0;
 }
