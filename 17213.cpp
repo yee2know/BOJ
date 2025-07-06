1b102ada6bada5e5d9c096d4e92d1f2c869ac466
wjh <wjh061016@gmail.com>
2025-07-06 21:19:27 +0900

    250706

---
+++ 17213.cpp
@@ -0,0 +1,25 @@
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int C(int a,int b){
+	long long num=1;
+	for(int i=0;i<b;i++){
+		num *= a-i;
+	}
+	for(int i=2;i<=b;i++){
+		num/=i;
+	}
+	return num;
+}
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	cout.tie(0);
+	int N,M;
+	cin >> N >> M;
+	cout << C(M-1,N-1);
+	return 0;
+}
